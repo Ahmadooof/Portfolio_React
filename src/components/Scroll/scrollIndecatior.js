4539a434a4fdf208e6173cd167be15cb8da89ab9
Ahmad Anbarje <ahmad.a.93@hotmail.com>
2021-02-15 19:39:16 +0100

    +change navbar+footer color +add scroll to section by click

---
+++ src/components/Scroll/scrollIndecatior.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function scrollIndecatior() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default scrollIndecatior
--- src/components/yearsSection/YearSection.js
+++ src/components/yearsSection/YearSection.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './YearSection.css'
 
 function YearSection() {
+    const commentSection = useRef(null);
+
+    const gotoCommentSection = () =>
+        window.scrollTo({
+            top: commentSection.current.offsetTop,
+            behavior: "smooth"
+        });
+
     return (
         <>
             <div className="nav-container">
                 <nav>
-                    <ul className="li">2021</ul>
+                    <ul className="li" ref={commentSection} onClick={gotoCommentSection}>2021</ul>
                     <ul className="li">2020</ul>
                     <ul className="li">2019</ul>
                 </nav>
