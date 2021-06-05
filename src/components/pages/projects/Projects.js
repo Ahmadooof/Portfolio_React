3c308c61dd2ba99107a1f1d3f966da0137dd47f1
Ahmad Anbarje <ahmad.a.93@hotmail.com>
2021-06-05 19:24:27 +0200

    change with nginx config

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../../App.css';
 import Cards from './../../cards/Cards';
 import './Project.css';
-import { Link } from 'react-router-dom';
 
 export default function Projects() {
 
