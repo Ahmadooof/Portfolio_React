ad9c1a86f880928eeb8c658b37340ebdddc30c6a
ahmad anbarje <ahmad.a.93@hotmail.com>
2023-09-07 09:25:17 +0300

    fix links bugs

--- client/src/components/cards/CardItem.js
+++ client/src/components/cards/CardItem.js
@@ -4,25 +4,22 @@ import '../buttons/Button.css'
 import { Link } from 'react-router-dom';
 
 function CardItem(props) {
-
-
   return (
     <>
       <li className='cards__item'>
-        <Link to={{ pathname: props.path }} target="_blank" className='cards__item__link'>
+        <Link to={props.path} target="_blank" className='cards__item__link'>
           <figure className='cards__item__pic-wrap'>
             <img
               className='cards__item__img'
               alt="sfe"
               src={props.src}
-
             />
           </figure>
           <div className='cards__item__info'>
             <div className="project-info">
               <h4 className='cards__item__text'>{props.text}</h4>
               <p className="daw">{props.description}</p>
-              <Link to={{ pathname: props.sourceCode }} target="_blank">
+              <Link to={props.sourceCode} target="_blank">
                 <button className="btn btn--primary">
                   View source code
                 </button>
@@ -31,7 +28,7 @@ function CardItem(props) {
             {props.svg}
           </div>
         </Link>
-      </li >
+      </li>
     </>
   );
 }
